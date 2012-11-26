7ed6b693e0d6bdfb5553a1534fefa94bece02277
Alejandro Mongua <alejandro.mongua@gmail.com>
2012-11-26 12:53:39 -0800

    Modificado el deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to, "/home/alejomongua/webapps/testapp"
 role :web, "web312.webfaction.com"                          # Your HTTP server, Apache/etc
 role :app, "web312.webfaction.com"                          # This may be the same as your `Web` server
 role :db,  "web312.webfaction.com", :primary => true # This is where Rails migrations will run
-# set :passphrase, "Resistencia86"
+set :passphrase, "Resistencia86"
 # role :db,  "your slave db-server here"
 set :user, "alejomongua"
 #set :scm_passphrase, "Resistencia86"
