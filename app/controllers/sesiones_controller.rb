05523513807b3bd4fd93c175c3f11e573c4a44ee
Alejandro Mongua <alejandro.mongua@gmail.com>
2012-11-27 22:26:58 -0500

    login con facebook

--- app/controllers/sesiones_controller.rb
+++ app/controllers/sesiones_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class SesionesController < ApplicationController
   def new
   end
@@ -11,6 +13,8 @@ class SesionesController < ApplicationController
   def fb_callback
     if params.has_key?(:code)
       session[:code] = params[:code]
+      auth_url = "https://graph.facebook.com/oauth/access_token?client_id=#{Testapp::Application::APP_ID}&redirect_uri=#{root_path}&client_secret=#{Testapp::Application::APP_SECRET}&code=#{params[:code]}"
+      session[:user] = JSON.parse(open(auth_url).read)
     else
       redirect_to 'https://www.facebook.com/dialog/oauth?client_id=121617807996302&redirect_uri=http%3A%2F%2Falejomongua.webfactional.com%2Ffacebook_callback&scope=email'      
     end
